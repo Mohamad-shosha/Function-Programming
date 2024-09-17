b7ef715f6e5652ed763fa38c2b6cb33c891f36f6
Mohamed shosha <150439621+Mohamad-shosha@users.noreply.github.com>
2024-09-17 03:49:38 +0300

    Add files via upload

---
+++ functionprogramming/src/main/java/com/shosha/springboot/demo/util/UnitFunctions.java
@@ -0,0 +1,17 @@
+package com.shosha.springboot.demo.util;
+
+public class UnitFunctions {
+
+    public static Integer checkNumber(int number) {
+        if (number < 0) {
+            throw new NumberFormatException();
+        } else {
+            return number;
+        }
+    }
+
+    public static String checkTimeOut() throws InterruptedException {
+        Thread.sleep(2000);
+        return "He is sleep now";
+    }
+}
---
+++ functionprogramming/src/test/java/com/shosha/springboot/demo/calculatortest/CalculatorTest.java
@@ -0,0 +1,71 @@
+package com.shosha.springboot.demo.calculatortest;
+
+import com.shosha.springboot.demo.calculator.Calculator;
+import org.junit.jupiter.api.*;
+
+import static com.shosha.springboot.demo.calculator.Calculator.*;
+
+@DisplayNameGeneration(DisplayNameGenerator.Simple.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class CalculatorTest {
+
+    @BeforeAll
+    static void setupBeforeEachClass() {
+        System.out.println("@BeforeAll executes only once before all test methods execution in the class\n");
+    }
+
+    @BeforeEach
+    public void setUp() {
+        System.out.println("Before each test, create instance of Calculator");
+        Calculator calculator = new Calculator();
+    }
+
+    @AfterAll
+    static void tearDownAfterAll() {
+        System.out.println("@AfterAll executes only once after all test methods execution in the class");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.out.println("Test is done successfully\n");
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("Test Addition")
+    public void testAddition() {
+        System.out.println("Running equals and not equals method");
+        Integer result = 20;
+        Assertions.assertEquals(result, add(15, 5), "result must be 20");
+        Assertions.assertNotEquals(result, add(1, 5), "result must be 20");
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Test Subtraction")
+    public void testSubtraction() {
+        System.out.println("Running equals and not equals method");
+        Integer result = 10;
+        Assertions.assertEquals(result,subtract(15, 5), "result must be 20");
+        Assertions.assertNotEquals(result, subtract(1, 5), "result must be 20");
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Test Multiplication")
+    public void testMultiplication() {
+        System.out.println("Running equals and not equals method");
+        Integer result = 20;
+        Assertions.assertEquals(result,multiply(2,10), "result must be 20");
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Test Division")
+    public void testDivision() {
+        System.out.println("Running equals and not equals method");
+        Float result = 4f;
+        Assertions.assertEquals(result,divide(10,2), "result must be 20");
+    }
+
+}
